099a8ae2696cb078a24b14b9d829cf772bf26765
Yashvi Suvariya <120238054+22CS082@users.noreply.github.com>
2024-07-28 14:44:54 +0530

    Update auth-middleware.js

--- server/middlewares/auth-middleware.js
+++ server/middlewares/auth-middleware.js
@@ -9,11 +9,11 @@ const authMiddleware = async (req, res, next) => {
   }
 
   const jwtToken = token.replace('Bearer ', '').trim();
-  const JWT_SECRET_KEY = "";//add secret key
+  
 
   try {
     console.log('JWT Token:', jwtToken);  // Debugging: Log the received token
-    const isVerified = jwt.verify(jwtToken, JWT_SECRET_KEY);
+    const isVerified = jwt.verify(jwtToken, process.env.JWT_SECRET_KEY);
     console.log('Verified Payload:', isVerified);  // Debugging: Log the verified payload
     const userData = await User.findOne({ email: isVerified.email }).select({ password: 0 });
 
