e4283eeab2448d52d2f4d1fdad88f6e9a89d44e5
Yashvi Suvariya <120238054+22CS082@users.noreply.github.com>
2024-07-28 14:43:58 +0530

    Update user_model.js

--- server/models/user_model.js
+++ server/models/user_model.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET_KEY = "" //add your secret key;
 
 const userSchema = new mongoose.Schema({
   username: {
